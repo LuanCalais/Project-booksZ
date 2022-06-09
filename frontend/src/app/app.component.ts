3f9c3ed435c79f263e188fa25e0acf02334a0fc5
LuanCalais <luancalais@gmail.com>
2022-06-09 13:44:06 -0300

    sidenav construida

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<app-header></app-header><app-footer></app-footer>',
+  template: '<app-header></app-header><app-nav></app-nav><app-footer></app-footer>',
   styles: []
 })
 export class AppComponent {
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,19 +5,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/template/header/header.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
 import { FooterComponent } from './components/template/footer/footer.component';
+import { NavComponent } from './components/template/nav/nav.component';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
+    NavComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule //Import toolbar angular material
+    MatToolbarModule, //Import toolbar angular material
+    MatSidenavModule, // Import Side Nav Module
+    MatCardModule, // Import Cards 
+    MatListModule // Import List
   ],
   providers: [],
   bootstrap: [AppComponent]

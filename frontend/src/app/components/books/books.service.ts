1fa3e4e24327fddfa49070456534d9f83332b8b3
LuanCalais <luancalais@gmail.com>
2022-06-13 21:30:13 -0300

    metodo na service para Read criad

---
+++ frontend/src/app/components/books/books.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http'; //Import para requisições http
+import { catchError, map, Observable } from 'rxjs';//Import de Observable
+import { Books } from './books';
+
+@Injectable({
+  providedIn: 'root'//Injetável na raiz
+})
+export class BooksService {
+
+  // String de base de url para chamadas no backend
+  baseUrl = "http://localhost:3001/books";
+
+  // Construtor levando um http para requisições
+  constructor(private http: HttpClient) { }
+
+  // Método da service que irá fazer a Read no backend
+  read(): Observable<Books[]>{
+    return this.http.get<Books[]>(this.baseUrl);
+  }
+
+
+
+}
